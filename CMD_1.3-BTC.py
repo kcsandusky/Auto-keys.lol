5ac8599f31810701a3beb0bfaabc7a8f9832e50a
lolapus <37475791+lolapus@users.noreply.github.com>
2019-11-12 09:32:23 -0600

    Removed multi-threading and fixed major problems
    Multi-threading was removed but the program speed has improved greatly

--- CMD_1.3-BTC.py
+++ CMD_1.3-BTC.py
@@ -1,29 +1,33 @@
-import urllib.request
-import time
 import sys
 import os
 import re
-import threading
+from requests_html import HTMLSession
+import random
 
 pages = 0
-print("Loaded Auto keys.lol CMD VERSION 1.3")
-threadCount = input('How many threads to run: ')
+print("Loaded Auto keys.lol bitcoin CMD VERSION 1.3")
 print("Started search...")
 
 
 def run():
     try:
         while 5 > 1:
-            webUrl = urllib.request.urlopen('https://keys.lol/bitcoin/random')
-            time.sleep(5)
-            data = webUrl.read()
+            #webUrl = urllib.request.urlopen('https://keys.lol/ethereum/1')
+            pageNum = random.randrange(1,904625697166532776746648320380374280100293470930272690489102837043110636675)
+            fullurl = 'https://keys.lol/bitcoin/'+str(pageNum)
+            session = HTMLSession()
+            webUrl = session.get(fullurl)
+            #time.sleep(10)
+            webUrl.html.render()
+            #html = webUrl.content
+            data = webUrl.html.html
             result = re.findall("[+-]?\d+\.\d+", str(data))
             for i in result:
                 if str(i) + " btc" in str(data):
                     if float(i) > 0:
-                        with open('ValidWallets.txt', 'a') as appendFile:
+                        with open('ValidWalletsBTC.txt', 'a') as appendFile:
                             appendFile.write('{} btc\n'.format(str(i)))
-                            appendFile.write('{}\n'.format(webUrl.geturl()))
+                            appendFile.write('{}\n'.format(fullurl))
             global pages
             pages = pages + 1
             sys.stdout.write("\rPages read: {}".format(str(pages)))
@@ -32,6 +36,7 @@ def run():
         os.execv(sys.executable, ['python'] + sys.argv)
 
 
-for i in range(int(threadCount)):
-    thread = threading.Thread(target=run)
-    thread.start()
+#for i in range(int(threadCount)):
+    #thread = threading.Thread(target=run)
+    #thread.start()
+run()
--- CMD_1.3-ETH.py
+++ CMD_1.3-ETH.py
@@ -1,9 +1,8 @@
-import urllib.request
-import time
 import sys
 import os
 import re
-import threading
+from requests_html import HTMLSession
+import random
 
 pages = 0
 print("Loaded Auto keys.lol ethereum CMD VERSION 1.3")
@@ -14,16 +13,22 @@ print("Started search...")
 def run():
     try:
         while 5 > 1:
-            webUrl = urllib.request.urlopen('https://keys.lol/ethereum/random')
-            time.sleep(10)
-            data = webUrl.read()
+            #webUrl = urllib.request.urlopen('https://keys.lol/ethereum/1')
+            pageNum = random.randrange(1,904625697166532776746648320380374280100293470930272690489102837043110636675)
+            fullurl = 'https://keys.lol/ethereum/'+str(pageNum)
+            session = HTMLSession()
+            webUrl = session.get(fullurl)
+            #time.sleep(10)
+            webUrl.html.render()
+            #html = webUrl.content
+            data = webUrl.html.html
             result = re.findall("[+-]?\d+\.\d+", str(data))
             for i in result:
                 if str(i) + " eth" in str(data):
                     if float(i) > 0:
                         with open('ValidWalletsETH.txt', 'a') as appendFile:
                             appendFile.write('{} eth\n'.format(str(i)))
-                            appendFile.write('{}\n'.format(webUrl.geturl()))
+                            appendFile.write('{}\n'.format(fullurl))
             global pages
             pages = pages + 1
             sys.stdout.write("\rPages read: {}".format(str(pages)))
@@ -32,6 +37,7 @@ def run():
         os.execv(sys.executable, ['python'] + sys.argv)
 
 
-for i in range(int(threadCount)):
-    thread = threading.Thread(target=run)
-    thread.start()
+#for i in range(int(threadCount)):
+    #thread = threading.Thread(target=run)
+    #thread.start()
+run()
